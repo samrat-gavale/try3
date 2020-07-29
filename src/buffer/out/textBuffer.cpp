ad38affec9da7aba7e1a4007621ebce4700a8a53
Samrat Gavale <sagavale@microsoft.com>
2020-07-28 17:25:57 -0700

    Add file from microsoft terminal repo.

---
+++ src/buffer/out/textBuffer.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "precomp.h"
+
+#include "textBuffer.hpp"
+line 1
+
+line 2
+
+line 3
+line 3
+line 4
+line 5
+
+
+
+line 6
+
+line 7
+
+line 8
+line 9
+#include "precomp.h"
+
+#include "textBuffer.hpp"
+
+line 10
+line 11
+
+// Routine Description:
+    _currentAttributes{ defaultAttributes },
+    _cursor{ cursorSize, *this },
+    _storage{},
+    _unicodeStorage{},
+    _renderTarget{ renderTarget },
+
+
+#include "textBuffer.hpp"
+	if () 
+	{
+	_storage.emplace_back(static_cast<SHORT>(i), screenBufferSize.X, _currentAttributes, this);
+	}
+
+#include "precomp.h"
+
+#include "textBuffer.hpp"
+
+// Routine Description:
+// - Retrieves read-only cell iterator at the given buffer location
+
+
+// Routine Description:
+
